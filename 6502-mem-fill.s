4a2447ce6f1db2cf95e65c6745db1aefac69b8d9
JCantu311 <jcantu4489@gmail.com>
2026-07-27 09:19:01 -0500

    Initial Commit

--- 6502-mem-fill.s
+++ 6502-mem-fill.s
@@ -4,31 +4,31 @@
 .segment "IO"
 .segment "CODE"
 start:
-    LDX #$00
+    LDX #$00         ; Load X index variable to zeropage
     STX $00
-    LDX #$01
+    LDX #$01         ; Load Y index variable to zeropage
     STX $01
-    LDX #$F0
+    LDX #$F0         ; Load loop counter to zeropage
     STX $02
 loop:
-    LDY $00
-    LDX $01
-    LDA #$00
-    LDA #$65
-    STA $0300, y
-    LDA #$02
-    STA $0300, x
-    INC $00
-    INC $01
-    LDA $02
-    CMP $00
-    BEQ exit
-    INC $00
-    INC $01
-    LDA $02
-    CMP $00
-    BEQ exit
-    JMP loop
+    LDX $00          ; Load X index variable from zeropage
+    LDY $01          ; Load Y index variable from zeropage
+    LDA #$00         ; Reset accumulator
+    LDA #$65         ; Load "65" into accumulator
+    STA $0300, x     ; Store "65" into memory at $0300 + X
+    LDA #$02         ; Load "02" into accumulator
+    STA $0300, y     ; Store "02" into memory at $0300 + Y
+    INC $00          ; Increment X index variable
+    INC $01          ; Increment Y index variable
+    LDA $02          ; Load loop counter from zeropage
+    CMP $00          ; Compare loop counter with X index variable
+    BEQ exit         ; If equal, exit loop
+    INC $00          ; Increment X index variable, again
+    INC $01          ; Increment Y index variable, again
+    LDA $02          ; Load loop counter from zeropage, again
+    CMP $00          ; Compare loop counter with X index variable, again
+    BEQ exit         ; If equal, exit loop
+    JMP loop         ; Return to loop
 exit:
     BRK
 .segment "RESET"
