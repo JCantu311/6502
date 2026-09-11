30c5b6132b0577a9ccd6820d36928ecaed6e298e
JCantu311 <jcantu4489@gmail.com>
2026-09-11 13:57:47 -0500

    Continued

--- Lanuage Compiler/cykompiler.c
+++ Lanuage Compiler/cykompiler.c
@@ -48,9 +48,9 @@ input_specified:
         goto output_specified;
     }
 
+output_specified:
     char *arg2 = argv[4];
 
-output_specified:
     char *flag3 = argv[5];
 
     if ((strcmp(argv[5], "-t") == 0 || strcmp(argv[5], "--type") == 0) && (argv[6] == NULL)) {
@@ -60,9 +60,9 @@ output_specified:
         goto type_specified;
     }
 
+type_specified:
     char *arg3 = argv[6];
 
-type_specified:
 no_output_specified:
     char *file_extension = ".cyk"; 
 
