bdd3cf04743f55d3a56d939a5c781b53bfedd3c2
JCantu311 <jcantu4489@gmail.com>
2026-09-11 13:42:01 -0500

    Continued

--- Lanuage Compiler/cykompiler.c
+++ Lanuage Compiler/cykompiler.c
@@ -28,6 +28,9 @@ int main(int argc, char *argv[]) {
     } else if ((strcmp(argv[1], "-i") == 0 || strcmp(argv[1], "--input") == 0) && (strcmp(argv[2], NULL) == 0)) {
         printf("No input file specified, aborted. \n Exit code: 2\n");
         return 2;
+    } else {
+        printf("Invalid argument, aborted. \n Exit code: 5\n");
+        return 5;
     }
 
     char *file = strdup(argv[2]);
