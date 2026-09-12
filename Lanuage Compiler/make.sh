57f73aa2dde73d07528c98f557ea7855443e0d62
JCantu311 <155592365+JCantu311@users.noreply.github.com>
2026-09-12 15:04:13 -0500

    Update make.sh

--- Lanuage Compiler/make.sh
+++ Lanuage Compiler/make.sh
@@ -1,11 +1,17 @@
-if command -v gcc >/dev/null 2>&1; then
-    gcc -static cykompiler.c lex.c parse.c emit.c etc.c input.c -o cykompiler
-else
-    echo "GCC is not installed. Please install GCC to proceed."
-    echo "Arch: sudo pacman -S gcc"
-    echo "Debian/Ubuntu: sudo apt install gcc"
-    echo "Fedora: sudo dnf install gcc"
-    echo "macOS: brew install gcc"
-    exit 1
-fi
-
+for i in {1..2}
+do
+    if [-d ./bin]; then
+        if command -v gcc >/dev/null 2>&1; then
+            gcc -static cykompiler.c lex.c parse.c emit.c etc.c input.c -o ./bin/cykompiler
+        else
+            echo "GCC is not installed. Please install GCC to proceed."
+            echo "Arch: sudo pacman -S gcc"
+            echo "Debian/Ubuntu: sudo apt install gcc"
+            echo "Fedora: sudo dnf install gcc"
+            echo "macOS: brew install gcc"
+            exit 1
+        fi
+    else
+        mkdir ./bin
+    fi
+done
