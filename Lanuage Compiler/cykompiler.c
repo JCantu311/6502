cda072e4966baed178385953db70f45930b3de4e
JCantu311 <jcantu4489@gmail.com>
2026-09-13 02:44:33 -0500

    Continued

--- Lanuage Compiler/cykompiler.c
+++ Lanuage Compiler/cykompiler.c
@@ -140,9 +140,9 @@ no_output:
         inputs_fin[3] = inputs[7];
     }
 
-    // if(inputs[6] != NULL && inputs[7] != NULL) {
-    //     inputs_fin[4] = strcat(inputs[6], inputs[7]);
-    // }
+    if(inputs[6] != NULL && inputs[7] != NULL) {
+        inputs_fin[4] = strcat(inputs[6], inputs[7]);
+    }
 
     printf("\n\n");
 
