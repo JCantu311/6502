cb632cc521422d956167714868f08eb9c21e7f78
JCantu311 <jcantu4489@gmail.com>
2026-09-14 20:49:54 -0500

    Continued

--- Lanuage Compiler/lex.c
+++ Lanuage Compiler/lex.c
@@ -1,5 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lex.h"
+#include "parse.h"
+#include "emit.h"
+#include "input.h"
+#include "etc.h"
+
+#ifdef _WIN32
+    #include <io.h>
+    #include <process.h>
+#else
+    #include <unistd.h>
+#endif
+
 
 int lex(char *inputs[], int size) {
+    for(int i = 0; i < size; i++) {
+        printf("%s\n", inputs[i] ? inputs[i] : "NULL");
+    }
     return 0;
 }
