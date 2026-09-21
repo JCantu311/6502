526d76dadc6493993e6cbcfdf82008e43c012c90
JCantu311 <jcantu4489@gmail.com>
2026-09-20 19:32:27 -0500

    Continued

--- Lanuage Compiler/parse.c
+++ Lanuage Compiler/parse.c
@@ -65,7 +65,7 @@ int parse(char *input_file_name) {
 
     while(fgets(buffer, sizeof(buffer), input) != NULL) {
         if ((buffer[0] == 't') && isdigit(buffer[1])) {
-            fprintf(tmp, "%s", &buffer[1]);
+            fprintf(tmp, "%s\n", &buffer[1]);
         } 
     }
 
